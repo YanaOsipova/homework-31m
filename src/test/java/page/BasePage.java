3480e2534a32f4605d6e8243bc679759a01c2ece
YanaOsipova <yana.osipova.prum@gmail.com>
2022-09-16 13:36:54 +0700

    2

--- src/test/java/page/BasePage.java
+++
@@ -1,9 +0,0 @@
-package page;
-
-import org.openqa.selenium.WebDriver;
-
-public class BasePage {
-    public void open(WebDriver webDriver) {
-        webDriver.get(System.getProperty("page"));
-    }
-}
